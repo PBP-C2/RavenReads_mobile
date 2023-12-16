aca932c8e96f90f0cbc2fe6fffc3c4e85f502646
shafiroar <shafiranrm@gmail.com>
2023-12-16 17:58:47 +0700

    Mencoba integrasi

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const LeftDrawer(),
+      // appBar: CurvedAppBar(),
+      // drawer: const LeftDrawer(),
       body: SingleChildScrollView(
         child: Container(
           color: const Color.fromARGB(255, 12, 39, 61), // Dark background color
@@ -130,79 +131,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
-    
-    // return Material(
-    //   color: item.color,
-    //   child: InkWell(
-    //     // Area responsive terhadap sentuhan
-    //     onTap: () async {
-    //       // Memunculkan SnackBar ketika diklik
-    //       ScaffoldMessenger.of(context)
-    //         ..hideCurrentSnackBar()
-    //         ..showSnackBar(SnackBar(
-    //             content: Text("Kamu telah menekan tombol ${item.name}!")));
-    //       if (item.name == "Discussion Forum") {
-    //         Navigator.push(
-    //             context,
-    //             MaterialPageRoute(
-    //                 builder: (context) => const MainDiscussion()));
-    //       } else if (item.name == "Magic Quiz") {
-    //         Navigator.push(
-    //             context, MaterialPageRoute(builder: (context) => QuizPage()));
-    //       } else if (item.name == "Spell Book") {
-    //         Navigator.push(context,
-    //             MaterialPageRoute(builder: (context) => const ShopFormPage()));
-    //       } else if (item.name == "Whole Scroll") {
-    //         Navigator.push(context,
-    //             MaterialPageRoute(builder: (context) => ProductListPage()));
-    //       } else if (item.name == "Book Store") {
-    //         Navigator.push(context,
-    //             MaterialPageRoute(builder: (context) => const BookStorePage()));
-    //       } else if (item.name == "Logout") {
-    //         final response = await request.logout(
-    //             "https://ravenreads-c02-tk.pbp.cs.ui.ac.id//auth/logout/");
-    //         String message = response["message"];
-    //         if (response['status']) {
-    //           String uname = response["username"];
-    //           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    //             content: Text("$message Sampai jumpa, $uname."),
-    //           ));
-    //           Navigator.pushReplacement(
-    //             context,
-    //             MaterialPageRoute(builder: (context) => const LoginPage()),
-    //           );
-    //         } else {
-    //           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    //             content: Text("$message"),
-    //           ));
-    //         }
-    //       }
-    //     },
-    //     child: Container(
-    //       // Container untuk menyimpan Icon dan Text
-    //       padding: const EdgeInsets.all(8),
-    //       child: Center(
-    //         child: Column(
-    //           mainAxisAlignment: MainAxisAlignment.center,
-    //           children: [
-    //             Icon(
-    //               item.icon,
-    //               color: Colors.white,
-    //               size: 30.0,
-    //             ),
-    //             const Padding(padding: EdgeInsets.all(3)),
-    //             Text(
-    //               item.name,
-    //               textAlign: TextAlign.center,
-    //               style: const TextStyle(color: Colors.white),
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
-  }
+  } 
 }
 
 class ShopItem {
